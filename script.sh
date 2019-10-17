dcaf4ef430f9dc0c563462ca5dea66a511195d73
Greg <23417426+8032@users.noreply.github.com>
2019-10-17 19:10:18 +0100

    install openssl

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ ls -la
 echo "--- :evergreen_tree: Build job environment"
 
 env
-
+apk add --update openssl
 echo "--- Setup kubectl"
 echo "Installing kubectl"
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
