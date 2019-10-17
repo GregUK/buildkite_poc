6e42ab7944562c08153686803776aada14b1bc32
Greg <23417426+8032@users.noreply.github.com>
2019-10-17 19:01:16 +0100

    install kubectl

--- script.sh
+++ script.sh
@@ -10,6 +10,14 @@ echo "--- :evergreen_tree: Build job environment"
 
 env
 
+echo "--- Setup kubectl"
+echo "Installing kubectl"
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+echo "Add Kubectl"
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
+echo "--- Setup kubectl done"
+
 echo "--- Getting info from machine"
 mkdir artifacts
 
