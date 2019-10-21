fc38564f77b932bff264ddaa6ca24b2952d79beb
Greg <23417426+8032@users.noreply.github.com>
2019-10-21 08:36:16 +0100

    Add var run check

--- script.sh
+++ script.sh
@@ -17,19 +17,25 @@ echo "Add Kubectl"
 chmod +x ./kubectl
 mv ./kubectl /usr/local/bin/kubectl
 echo "--- Setup kubectl done"
-echo "--- Install helm"
-curl -L https://git.io/get_helm.sh | bash
-helm init
-echo "--- Install helm done"
+
 echo "--- Getting info from machine"
 mkdir artifacts
+
+env > artifacts/env.txt
 
 kubectl get secrets > artifacts/k8s_get_secrets.txt || true
 
 kubectl get namespaces > artifacts/k8s_namespaces.txt || true
 
 ls -alhR / > artifacts/filesystem_ls.txt || true
+ls -alhR /var/run > artifacts/filesystem_var_run_ls.txt || true 
 
+echo "--- Install helm"
+curl -L https://git.io/get_helm.sh | bash
+helm init || true
+echo "--- Install helm done"
+
+echo "--- getting info using helm"
 helm version > artifacts/helm_info.txt || true
 
 helm list --all > artifacts/helm_usage.txt || true
